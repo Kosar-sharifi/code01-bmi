8bf5363860f45b8d0df4dad22eee449f36109908
kosar-sharifi <kosarsharifi715@gmail.com>
2024-08-20 21:36:38 +0330

    edite code01

--- code01-bmi.py
+++ code01-bmi.py
@@ -1,6 +1,5 @@
 w = float(input("enter your weight:"))
 h = float(input("enter your height:"))
 bmi = w/(h**2)
-
 print(f"your weight is {w} and your height is {h} so bmi is {bmi:.2f}")
 print(f"your weight is {w} and your height is {h} so bmi is {round(bmi,2)}")
